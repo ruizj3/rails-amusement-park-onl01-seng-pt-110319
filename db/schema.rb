2ac21f971e103641af85cbdd533556073d88a13f
Joseph Ruiz <ruizj3@uw.edu>
2020-05-02 21:13:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_205829) do
+ActiveRecord::Schema.define(version: 2020_05_02_211322) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_205829) do
     t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
 end
