9f73e2d1f80e020d811111cb4bbd65dfe9e2ce86
Joseph Ruiz <ruizj3@uw.edu>
2020-05-02 20:57:47 +0000

    Automatically backed up by Learn

--- db/migrate/20200502205615_create_rides.rb
+++ db/migrate/20200502205615_create_rides.rb
@@ -1,6 +1,14 @@
 class CreateRides < ActiveRecord::Migration[5.2]
   def change
     create_table :rides do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
+
+
+      t.timestamps null: false
     end
   end
 end
