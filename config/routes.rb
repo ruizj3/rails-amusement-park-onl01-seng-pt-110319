a34d3775412686943b3048813ea65d3028254166
Joseph Ruiz <ruizj3@uw.edu>
2020-05-02 21:39:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
   get '/users/new', to: 'users#new', as: 'new_user'
-  root 'static#home'
   get '/users', to:'users#index', as: 'users'
   post '/users', to:'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
